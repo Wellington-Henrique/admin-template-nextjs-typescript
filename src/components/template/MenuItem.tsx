13ab34806f62acd424318c857cc666b632bdc2be
Wellington Henrique <wellingtonhlc@gmail.com>
2023-09-28 22:31:22 -0300

    169. Componente Menu Lateral #2

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,17 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
 
 interface MenuItemProps {
-    url: string
-    texto: string
-    icone: any
+  url: string
+  texto: string
+  icone: any
 }
 
 export default function MenuItem(props: MenuItemProps) {
   return (
     <li className={`
-
+    hover:bg-gray-100 
     `}>
+      <Link href={props.url} className={`
+        flex flex-col justify-center items-center
+        h-20 w-20 
+      `}>
         {props.icone}
+        <span className={`
+          text-xs font-light text-gray-600
+        `}>{props.texto}</span>
+      </Link>
     </li>
   )
 }
