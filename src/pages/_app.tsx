8520aad9e7ee550b3e5f2ebc4e88e3e8ab9261e2
Wellington Henrique <wellingtonhlc@gmail.com>
2023-10-02 22:13:59 -0300

    175. Context API #1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
+import { AppProvider } from '@/data/context/AppContext'
 import type { AppProps } from 'next/app'
 
+import '@/styles/globals.css'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AppProvider>
+      <Component {...pageProps} />
+    </AppProvider>
+  )
 }
--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,12 +1,15 @@
 import Layout from "@/components/template/Layout";
+import useAppData from "@/data/hook/useAppData";
 
 export default function Notificacoes() {
-    return (
-      <Layout
-        titulo="Notificações"
-        subtitulo="Aqui você irá gerenciar as suas notificações!"
-      >
-        <h3>Notificações</h3>
-      </Layout>
-    )
+  const ctx = useAppData();
+
+  return (
+    <Layout
+      titulo="Notificações"
+      subtitulo="Aqui você irá gerenciar as suas notificações!"
+    >
+      <h3>{ctx.nome}</h3>
+    </Layout>
+  )
 }
